2999bf2d8559d96551671e645128e0b3c488c7d8
EvgeniiG <7ee2cc898cca1b5fc49df740c2081dfc681e0a28>
2019-06-05 14:03:31 -0700

    Convert comments into tooltips

--- com.unity.render-pipelines.high-definition/Runtime/Sky/PbrSky/PbrSkySettings.cs
+++ com.unity.render-pipelines.high-definition/Runtime/Sky/PbrSky/PbrSkySettings.cs
@@ -6,38 +6,35 @@ namespace UnityEngine.Experimental.Rendering.HDPipeline
     public class PbrSkySettings : SkySettings
     {
         /* We use the measurements from Earth as the defaults. */
-        // Radius of the planet (distance from the core to the sea level). Units: km.
+        [Tooltip("Radius of the planet (distance from the core to the sea level). Units: km.")]
         public MinFloatParameter planetaryRadius = new MinFloatParameter(6378.759f, 0);
-        // Position of the center of the planet in the world space. Does not affect the precomputation.
+        [Tooltip("Position of the center of the planet in the world space. Units: km.")]
+        // Does not affect the precomputation.
         public Vector3Parameter planetCenterPosition = new Vector3Parameter(new Vector3(0, -6378.759f, 0));
-        // Extinction coefficient of air molecules at the sea level. Units: 1/(1000 km).
+        [Tooltip("Extinction coefficient of air molecules at the sea level. Units: 1/(1000 km).")]
         // TODO: use mean free path?
         public ColorParameter airThickness = new ColorParameter(new Color(5.8f, 13.5f, 33.1f), hdr: true, showAlpha: false, showEyeDropper: false);
-        // Single scattering albedo of air molecules.
-        // It is the ratio between the scattering and the extinction coefficients.
-        // The value of 0 results in absorbing molecules, and the value of 1 results in scattering ones.
+        [Tooltip("Single scattering albedo of air molecules. Ratio between the scattering and the extinction coefficients. The value of 0 results in absorbing molecules, and the value of 1 results in scattering ones.")]
         // Note: this allows us to account for absorption due to the ozone layer.
         // We assume that ozone has the same height distribution as air (CITATION NEEDED!).
         public ColorParameter airAlbedo = new ColorParameter(new Color(0.9f, 0.9f, 1.0f), hdr: false, showAlpha: false, showEyeDropper: false);
-        // Exponential falloff of air density w.r.t. height. (Rayleigh). Units: 1/km.
+        [Tooltip("Exponential falloff of air density w.r.t. height. Units: 1/km.")]
         public ClampedFloatParameter airDensityFalloff = new ClampedFloatParameter(1.0f / 8.434f, 0, 1);
-        // Extinction coefficient of aerosol molecules at the sea level. Units: 1/(1000 km).
+        // TODO: use scale height?
+        [Tooltip("Extinction coefficient of aerosol molecules at the sea level. Units: 1/(1000 km).")]
         // Note: aerosols are (fairly large) solid or liquid particles in the air.
         // TODO: use mean free path?
         public MinFloatParameter aerosolThickness = new MinFloatParameter(2.0f, 0);
-        // Single scattering albedo of aerosol molecules.
-        // It is the ratio between the scattering and the extinction coefficients.
-        // The value of 0 results in absorbing molecules, and the value of 1 results in scattering ones.
+        [Tooltip("Single scattering albedo of aerosol molecules. Ratio between the scattering and the extinction coefficients. The value of 0 results in absorbing molecules, and the value of 1 results in scattering ones.")]
         public ClampedFloatParameter aerosolAlbedo = new ClampedFloatParameter(0.9f, 0, 1);
-        // Exponential falloff of aerosol density w.r.t. height. (Mie). Units: 1/km.
+        [Tooltip("Exponential falloff of aerosol density w.r.t. height. Units: 1/km.")]
+        // TODO: use scale height?
         public ClampedFloatParameter aerosolDensityFalloff = new ClampedFloatParameter(1.0f / 1.2f, 0, 1);
-        // +1: forward  scattering.
-        //  0: almost isotropic.
-        // -1: backward scattering.
+        [Tooltip("+1: forward  scattering. 0: almost isotropic. -1: backward scattering.")]
         public ClampedFloatParameter aerosolAnisotropy = new ClampedFloatParameter(0, -1, 1);
-        // Number of the scattering events.
+        [Tooltip("Number of the scattering events.")]
         public ClampedIntParameter numBounces = new ClampedIntParameter(8, 1, 10);
-        // Albedo of the planetary surface.
+        [Tooltip("Albedo of the planetary surface.")]
         public ColorParameter groundColor = new ColorParameter(new Color(0.4f, 0.25f, 0.15f), hdr: false, showAlpha: false, showEyeDropper: false);
         // Hack. Does not affect the precomputation.
         public CubemapParameter groundAlbedoTexture = new CubemapParameter(null);
